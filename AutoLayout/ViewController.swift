13d4fdd79e182055056e6f74723f70346a0a9d39
Lucas <lucas@agtlucas.com>
2015-03-03 14:19:32 -0300

    Adding IBOutlet for the views

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topLeftView: UIView!
+    @IBOutlet weak var topRightView: UIView!
+    @IBOutlet weak var bottomView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
